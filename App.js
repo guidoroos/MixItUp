883bcd679824b247fc6b45cc9932122941a01552
guido.roos <guido.roos@iodigital.com>
2025-12-01 13:10:32 +0100

    home page and init

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getAllCocktails } from './api/CocktailApi';
+import { initializeDatabase, insertCocktail, isCocktailTableEmpty } from './db/Database';
+import { useEffect, useState } from 'react';
+import Navigation from './Navigation';
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    const initializeDB = async () => {
+      try {
+        await initializeDatabase();
+        
+        if (isCocktailTableEmpty()) {
+          const cocktails = await getAllCocktails();
+          cocktails.forEach(cocktail => {
+            insertCocktail(cocktail);
+          });
+        }
+        setDbInitialized(true);
+      } catch (error) {
+        console.error('Database initialization failed:', error);
+      }
+    };
+
+    initializeDB();
+  }, []);
+
+  console.log('DB Initialized:', dbInitialized);
+  if (!dbInitialized) {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Navigation />
     </View>
   );
 }
@@ -14,7 +50,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
